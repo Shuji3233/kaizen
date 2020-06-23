58e11207e2652b1316a5180f3f5678766857739e
Shuji3233 <gorio9999@gmail.com>
2020-06-23 17:49:12 +0900

    Add Navigation and user_id_column_to_posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_22_213453) do
+ActiveRecord::Schema.define(version: 2020_06_23_084408) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_06_22_213453) do
     t.string "benefits"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
